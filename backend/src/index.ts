c03bbaba8ac7712c294093afabe50f992e84fa09
kajing1077 <kajing1077@gmail.com>
2024-04-11 19:18:33 +0900

    remove dotenv configuration and unused imports

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,5 @@
 import { app } from "./app";
 import { PORT } from "./settings";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
